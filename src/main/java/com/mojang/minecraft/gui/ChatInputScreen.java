ed93fe863d847fc277d069eb1f687f164051e461
Jonty800 <Jonty800@gmail.com>
2013-08-19 17:08:08 +0100

    Added Up/Down to show last chat messages (Needs testing on network)

--- src/main/java/com/mojang/minecraft/gui/ChatInputScreen.java
+++ src/main/java/com/mojang/minecraft/gui/ChatInputScreen.java
@@ -1,68 +1,105 @@
 package com.mojang.minecraft.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mojang.minecraft.net.NetworkManager;
 import com.mojang.minecraft.net.PacketType;
 import org.lwjgl.input.Keyboard;
 
 public final class ChatInputScreen extends GuiScreen {
 
-   public String message = "";
-   private int counter = 0;
+	public String message = "";
+	private int counter = 0;
 
+	public final void onOpen() {
+		Keyboard.enableRepeatEvents(true);
+	}
 
-   public final void onOpen() {
-      Keyboard.enableRepeatEvents(true);
-   }
+	public final void onClose() {
+		Keyboard.enableRepeatEvents(false);
+	}
 
-   public final void onClose() {
-      Keyboard.enableRepeatEvents(false);
-   }
+	public final void tick() {
+		++this.counter;
+	}
 
-   public final void tick() {
-      ++this.counter;
-   }
+	List<String> typinglog = new ArrayList<String>();
+	int typinglogpos = 0;
 
-   protected final void onKeyPress(char var1, int var2) {
-      if(var2 == 1) {
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-      } else if(var2 == 28) {
-         NetworkManager var10000 = this.minecraft.networkManager;
-         String var4 = this.message.trim();
-         NetworkManager var3 = var10000;
-         if((var4 = var4.trim()).length() > 0) {
-            var3.netHandler.send(PacketType.CHAT_MESSAGE, new Object[]{Integer.valueOf(-1), var4});
-         }
+	protected final void onKeyPress(char var1, int var2) {
+		if (var2 == 1) {
+			this.minecraft.setCurrentScreen((GuiScreen) null);
+		} else if (var2 == 28) {
+			NetworkManager var10000 = this.minecraft.networkManager;
+			String var4 = this.message.trim();
+			NetworkManager var3 = var10000;
+			if ((var4 = var4.trim()).length() > 0) {
+				var3.netHandler.send(PacketType.CHAT_MESSAGE, new Object[] {
+						Integer.valueOf(-1), var4 });
+				typinglog.add(this.message);
+				typinglogpos = typinglog.size();
+			}
 
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-      } else {
-         if(var2 == 14 && this.message.length() > 0) {
-            this.message = this.message.substring(0, this.message.length() - 1);
-         }
+			this.minecraft.setCurrentScreen((GuiScreen) null);
+		} else {
+			if (var2 == 14 && this.message.length() > 0) {
+				this.message = this.message.substring(0,
+						this.message.length() - 1);
+			}
 
-         if("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.:-_\'*!\\\"#%/()=+?[]{}<>@|$;^`~".indexOf(var1) >= 0 && this.message.length() < 64 - (this.minecraft.session.username.length() + 2)) {
-            this.message = this.message + var1;
-         }
+			if (var2 == Keyboard.KEY_UP) {
+				typinglogpos--;
+				if (typinglogpos < 0) {
+					typinglogpos = 0;
+				}
+				if (typinglogpos >= 0 && typinglogpos < typinglog.size()) {
+					this.message = typinglog.get(typinglogpos);
+				}
+			}
+			if (var2 == Keyboard.KEY_DOWN) {
+				typinglogpos++;
+				if (typinglogpos > typinglog.size()) {
+					typinglogpos = typinglog.size();
+				}
+				if (typinglogpos >= 0 && typinglogpos < typinglog.size()) {
+					this.message = typinglog.get(typinglogpos);
+				}
+				if (typinglogpos == typinglog.size()) {
+					this.message = "";
+				}
+			}
 
-      }
-   }
+			if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.:-_\'*!\\\"#%/()=+?[]{}<>@|$;^`~"
+					.indexOf(var1) >= 0
+					&& this.message.length() < 64 - (this.minecraft.session.username
+							.length() + 2)) {
+				this.message = this.message + var1;
+			}
 
-   public final void render(int var1, int var2) {
-      drawBox(2, this.height - 14, this.width - 2, this.height - 2, Integer.MIN_VALUE);
-      drawString(this.fontRenderer, "> " + this.message + (this.counter / 6 % 2 == 0?"_":""), 4, this.height - 12, 14737632);
-   }
+		}
+	}
 
-   protected final void onMouseClick(int var1, int var2, int var3) {
-      if(var3 == 0 && this.minecraft.hud.hoveredPlayer != null) {
-         if(this.message.length() > 0 && !this.message.endsWith(" ")) {
-            this.message = this.message + " ";
-         }
+	public final void render(int var1, int var2) {
+		drawBox(2, this.height - 14, this.width - 2, this.height - 2,
+				Integer.MIN_VALUE);
+		drawString(this.fontRenderer, "> " + this.message
+				+ (this.counter / 6 % 2 == 0 ? "_" : ""), 4, this.height - 12,
+				14737632);
+	}
 
-         this.message = this.message + this.minecraft.hud.hoveredPlayer;
-         var1 = 64 - (this.minecraft.session.username.length() + 2);
-         if(this.message.length() > var1) {
-            this.message = this.message.substring(0, var1);
-         }
-      }
+	protected final void onMouseClick(int var1, int var2, int var3) {
+		if (var3 == 0 && this.minecraft.hud.hoveredPlayer != null) {
+			if (this.message.length() > 0 && !this.message.endsWith(" ")) {
+				this.message = this.message + " ";
+			}
 
-   }
+			this.message = this.message + this.minecraft.hud.hoveredPlayer;
+			var1 = 64 - (this.minecraft.session.username.length() + 2);
+			if (this.message.length() > var1) {
+				this.message = this.message.substring(0, var1);
+			}
+		}
+
+	}
 }
