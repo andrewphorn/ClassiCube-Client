2b3b15e1bcbef40b1aa187743d011bdd16bcb660
Jonty800 <Jonty800@gmail.com>
2013-08-28 19:04:13 +0100

    Finished packets AddSelectionBox and RemoveSelectionBox; tested and working. Fixed Onyx being missing from the block selection screen for default blocks

--- src/main/java/com/mojang/minecraft/ColorCache.java
+++ src/main/java/com/mojang/minecraft/ColorCache.java
@@ -5,13 +5,21 @@ public class ColorCache {
 	public float R;
 	public float G;
 	public float B;
+	public float A;
 	
 	public ColorCache(float r, float g, float b)
 	{
 		R = FixColor(r);
 		G = FixColor(g);
 		B = FixColor(b);
-		
+		A = 1F;
+	}
+	public ColorCache(float r, float g, float b, float a)
+	{
+		R = FixColor(r);
+		G = FixColor(g);
+		B = FixColor(b);
+		A = a;
 	}
 	float FixColor(float color){
 		if(color > 1) return 1F;
