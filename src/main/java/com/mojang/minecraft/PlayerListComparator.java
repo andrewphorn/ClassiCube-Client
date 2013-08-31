f210c362dab035a6481fca6d2cd2942e0a2d10f5
Jonty800 <Jonty800@gmail.com>
2013-08-31 04:51:02 +0100

    Fixed a potential glitch in playerList, playerList groups are now sorted by alphabetical order. changed lwjgl to unreal engine

---
+++ src/main/java/com/mojang/minecraft/PlayerListComparator.java
@@ -0,0 +1,12 @@
+package com.mojang.minecraft;
+
+import java.util.Comparator;
+
+public class PlayerListComparator implements Comparator<PlayerListNameData> {
+
+    @Override
+    public int compare(PlayerListNameData o1, PlayerListNameData o2) {
+	// TODO Auto-generated method stub
+	return o1.groupName.compareToIgnoreCase(o2.groupName);
+    }
+}
