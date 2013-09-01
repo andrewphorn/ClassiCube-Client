25844b00bd630e74d7387caf6969b847ef48176f
andrewphorn <andrewphorn@gmail.com>
2013-09-01 03:12:44 -0700

    oh haha I should learn to read
    Signed-off-by: andrewphorn <andrewphorn@gmail.com>

--- src/main/java/com/mojang/minecraft/player/InputHandlerImpl.java
+++ src/main/java/com/mojang/minecraft/player/InputHandlerImpl.java
@@ -14,12 +14,12 @@ public class InputHandlerImpl extends InputHandler {
     public void updateMovement() {
 
     // wow this shouldn't be neccesary.
-    keystate[0] = Keyboard.isKeyDown(settings.forwardKey)
-    keystate[1] = Keyboard.isKeyDown(settings.backKey)
-    keystate[2] = Keyboard.isKeyDown(settings.leftkey)
-    keystate[3] = Keyboard.isKeyDown(settings.rightKey)
-    keystate[4] = Keyboard.isKeyDown(settings.jumpKey)
-    keystate[5] = Keyboard.isKeyDown(settings.runKey)
+    keystate[0] = Keyboard.isKeyDown(settings.forwardKey);
+    keystate[1] = Keyboard.isKeyDown(settings.backKey);
+    keystate[2] = Keyboard.isKeyDown(settings.leftkey);
+    keystate[3] = Keyboard.isKeyDown(settings.rightKey);
+    keystate[4] = Keyboard.isKeyDown(settings.jumpKey);
+    keystate[5] = Keyboard.isKeyDown(settings.runKey);
 	xxa = 0.0F;
 	yya = 0.0F;
 
@@ -81,7 +81,7 @@ public class InputHandlerImpl extends InputHandler {
 	    index = 5;
 	}
 	if (index >= 0) {
-	    //keyStates[index] = state;
+	    keyStates[index] = state;
 	}
 
     }
