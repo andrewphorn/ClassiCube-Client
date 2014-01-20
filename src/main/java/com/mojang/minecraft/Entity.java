c8f400ccacf18de886bb71ecaf3227f7b0abccde
Gamemakergm <gamemakergmdev@gmail.com>
2014-01-20 16:35:35 -0700

    Formatted a bit.

--- src/main/java/com/mojang/minecraft/Entity.java
+++ src/main/java/com/mojang/minecraft/Entity.java
@@ -172,16 +172,6 @@ public abstract class Entity implements Serializable {
 
 	}
 
-	/**
-	 * Checks if this entity's bounding box intersects
-	 * @param var1
-	 * @param var2
-	 * @param var3
-	 * @param var4
-	 * @param var5
-	 * @param var6
-	 * @return
-	 */
 	public boolean intersects(float var1, float var2, float var3, float var4,
 			float var5, float var6) {
 		return bb.intersects(var1, var2, var3, var4, var5, var6);
