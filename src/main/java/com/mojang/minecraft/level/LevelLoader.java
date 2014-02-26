cc5f2ffc2e0acf043c81c71657ff15178204e543
123DMWM <shmo1joe2@gmail.com>
2014-02-26 12:12:49 -0500

    Umby24s' fix for level sizes when saving/loading
    Z and Y were swapped

--- src/main/java/com/mojang/minecraft/level/LevelLoader.java
+++ src/main/java/com/mojang/minecraft/level/LevelLoader.java
@@ -56,8 +56,8 @@ public class LevelLoader {
 		blocks = tc.getByteArray("BlockArray");
 
 		level.width = X;
-		level.height = Y;
-		level.depth = Z;
+		level.height = Z;
+		level.depth = Y;
 		level.blocks = blocks;
 
 		NBTTagCompound spawn = tc.getCompoundTag("Spawn");
