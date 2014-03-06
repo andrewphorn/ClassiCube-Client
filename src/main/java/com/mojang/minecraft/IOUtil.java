6db98e247d54069071ef3ff080f4b3a1b783f959
fragmer <me@matvei.org>
2014-03-05 18:51:10 -0700

    Fixed a number of bugs and inefficiencies in ResourceDownloadThread. Now the client can start normally in applet mode or from launcher, even if some resource files were missing. TODO: proper threading/synchronization with main thread, to avoid locking up the UI during download.

---
+++ src/main/java/com/mojang/minecraft/IOUtil.java
@@ -0,0 +1,38 @@
+package com.mojang.minecraft;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+
+public class IOUtil {
+    private static final int BUFFER_SIZE = 64 * 1024; // 64 KB
+
+    // Reads given stream to the end, and writes its contents to a file
+    public static void copyStreamToFile(InputStream inStream, File file)
+            throws IOException {
+        ReadableByteChannel in = Channels.newChannel(inStream);
+        try {
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            FileOutputStream outStream = new FileOutputStream(file);
+            try {
+                FileChannel out = outStream.getChannel();
+                long offset = 0;
+                long count;
+                while ((count = out.transferFrom(in, offset, BUFFER_SIZE)) > 0) {
+                    offset += count;
+                }
+            } finally {
+                outStream.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+}
