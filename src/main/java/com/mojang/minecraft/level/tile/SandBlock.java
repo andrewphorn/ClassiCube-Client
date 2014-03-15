10b7bd035c1c813e0e514e85d1c68517d6e6b088
tyteen4a03 <tyteen4a03@3pointstudio.com>
2014-03-15 16:42:33 +0100

    Some sorceries are to remain unfixed... (reverted SandBlock simplifications)

--- src/main/java/com/mojang/minecraft/level/tile/SandBlock.java
+++ src/main/java/com/mojang/minecraft/level/tile/SandBlock.java
@@ -14,31 +14,33 @@ public final class SandBlock extends Block {
         if (!Minecraft.isSinglePlayer) {
             return;
         }
-        int newY = y;
+        int yOffset = y;
 
         while (true) {
-            int below = newY - 1;
-            int tile = level.getTile(x, below, z);
-            LiquidType liquidType = blocks[tile].getLiquidType();
-            if (!(tile == 0 || (liquidType == LiquidType.water || liquidType == LiquidType.lava)) || newY <= 0) {
-                if (newY != y) {
-                    if ((tile = level.getTile(x, newY, z)) > 0
-                            && blocks[tile].getLiquidType() != LiquidType.notLiquid) {
-                        level.setTileNoUpdate(x, newY, z, 0);
+            int yCheckPos = yOffset - 1;
+            int nextTile;
+            LiquidType liquidType;
+            if (!((nextTile = level.getTile(x, yCheckPos, z)) == 0 ? true : (liquidType = blocks[nextTile]
+                    .getLiquidType()) == LiquidType.water ? true : liquidType == LiquidType.lava)
+                    || yOffset <= 0) {
+                if (yOffset != y) {
+                    if ((nextTile = level.getTile(x, yOffset, z)) > 0
+                            && blocks[nextTile].getLiquidType() != LiquidType.notLiquid) {
+                        level.setTileNoUpdate(x, yOffset, z, 0);
                     }
 
-                    level.swap(x, y, z, x, newY, z);
+                    level.swap(x, y, z, x, yOffset, z);
                 }
 
                 return;
             }
 
-            --newY;
+            --yOffset;
         }
     }
 
     @Override
-    public final void onNeighborChange(Level level, int x, int y, int z, int side) {
+    public final void onNeighborChange(Level level, int x, int y, int z, int unused) {
         fall(level, x, y, z);
     }
 
