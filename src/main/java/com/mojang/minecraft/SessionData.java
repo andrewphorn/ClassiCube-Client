cd98868ec5d53b6db0cd9b0b07636eb2e8b0cce2
tyteen4a03 <tyteen4a03@3pointstudio.com>
2014-03-20 01:42:51 +0100

    derp

--- src/main/java/com/mojang/minecraft/SessionData.java
+++ src/main/java/com/mojang/minecraft/SessionData.java
@@ -1,7 +1,7 @@
 package com.mojang.minecraft;
 
 import java.util.ArrayList;
-import java.util.Arrays
+import java.util.Arrays;
 import java.util.List;
 
 import com.mojang.minecraft.level.tile.Block;
