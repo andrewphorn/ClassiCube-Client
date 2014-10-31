2ac0114b0ab6de85af03c44f48e9c11f18aba54a
fragmer <me@matvei.org>
2014-10-30 20:53:59 -0600

    Committing missing Textures.java

---
+++ src/main/java/com/mojang/minecraft/render/texture/Textures.java
@@ -0,0 +1,20 @@
+package com.mojang.minecraft.render.texture;
+
+public final class Textures {
+    public static final String
+            PARTICLES = "/particles.png",
+            TERRAIN = "/terrain.png",
+            LOADING_BACKGROUND = "/dirt.png",
+            MAP_SIDE = "/rock.png",
+            MAP_EDGE = "/water.png",
+            FONT = "/default.png",
+            GUI = "/gui/gui.png",
+            ICONS = "/gui/icons.png",
+            ARROWS = "/item/arrows.png",
+            ARMOR_PLATE = "/armor/plate.png",
+            SHEEP_FUR = "/mob/sheep_fur.png",
+            HUMANOID_SKIN = "/char.png",
+            CLOUDS = "/clouds.png",
+            RAIN = "/rain.png",
+            SNOW = "/snow.png";
+}
